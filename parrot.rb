b67000ad85c1d849068df679aaf8b69bd21e312e
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-15 22:03:31 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+var sing = 'squak!'
+
+def parrot
+  "The parrot will #{sing}"
+  puts "The parrot will #{sing}"
+end  
+
+ 
+
+parrot
